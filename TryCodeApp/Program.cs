815db2f9a3da18720db91c459b52af6ea84c161d
Emircan <emircandv787@gmail.com>
2025-01-11 00:54:01 +0300

    The Factory Sample 1 has been added.

--- TryCodeApp/Program.cs
+++ TryCodeApp/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿#region Factory Sample 1
+
+using Creational.Factory.Sample1;
+using Creational.Factory.Sample1.Enums;
+using Creational.Factory.Sample1.Models;
+
+var userEventFactory = new UserEventFactory();
+
+//var userEvent = userEventFactory.CreateUserEvent(UserEventType.UserCreated);
+var userEvent = userEventFactory.CreateUserEvent(UserEventType.UserDeleted);
+
+if(userEvent is UserCreatedEvent userCreatedEvent)
+{
+    Console.WriteLine(userCreatedEvent.Id);
+    Console.WriteLine(userCreatedEvent.Name);
+    Console.WriteLine(userCreatedEvent.Age);
+    Console.WriteLine(userCreatedEvent.CreatedAt);
+}
+else
+{
+    Console.WriteLine("UserEvent is not UserCreatedEvent");
+}
+
+
+#endregion
